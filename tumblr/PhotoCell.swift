0e7db2afc1266fa2b025874017c9c84de98d38de
SandeshOnRails <basnetsandesh450@gmail.com>
2018-08-28 22:32:31 -0700

    table view added. images populated in the table view cells by making get request to tumblr api

---
+++ tumblr/PhotoCell.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoCell.swift
+//  tumblr
+//
+//  Created by Sandesh Basnet on 8/28/18.
+//  Copyright © 2018 Sandesh Basnet. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+    
+    
+   
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ tumblr/PhotosViewController.swift
@@ -0,0 +1,124 @@
+//
+//  PhotosViewController.swift
+//  tumblr
+//
+//  Created by Sandesh Basnet on 8/28/18.
+//  Copyright © 2018 Sandesh Basnet. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    var posts: [[String: Any]] = []
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        fetchData()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 250
+        
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+
+        
+        let  cell = tableView.dequeueReusableCell(withIdentifier:"PhotoCell") as! PhotoCell
+
+        
+        let post = self.posts[indexPath.row]
+        
+        if let photos = post["photos"] as? [[String: Any]] {
+            
+            let photo = photos[0]
+            
+            let original = photo["original_size"] as! [String: Any]
+            
+            let urlPath = original["url"] as! String
+            
+            let url = URL(string: urlPath)
+        
+            cell.posterImage?.af_setImage(withURL: url!)
+            
+            
+            
+        }
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    
+    func fetchData() {
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        
+           let request = URLRequest(url:url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 10)
+
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                
+                //print(dataDictionary)
+                
+                let response = dataDictionary ["response"] as! [String:Any]
+                
+                self.posts = response["posts"] as! [[ String: Any]]
+                
+                self.tableView.reloadData()
+                
+                
+            }
+            
+            
+        }
+        
+        
+        task.resume()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
